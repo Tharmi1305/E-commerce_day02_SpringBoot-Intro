a814d98cad55b94d452464a6db517d432e07c2f9
Tharmi01 <tharmihansenthur01@gmail.com>
2025-03-30 11:13:19 +0530

    AppController class completed

--- Basic/src/main/java/com/example/demo/Controller/AppController.java
+++ Basic/src/main/java/com/example/demo/Controller/AppController.java
@@ -1,5 +1,20 @@
 package com.example.demo.Controller;
 
-public class AppController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/app")
+public class AppController {
+	
+	@GetMapping("/msg")
+	public String myMessage() {
+		return "Hello SpringBoot";
+	}
+	
+	@GetMapping("/name")
+	public String myName() {
+		return "Tharmi First Project";
+	}
 }
